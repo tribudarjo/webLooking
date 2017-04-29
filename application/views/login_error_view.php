f64ec68a9c881f0e7d4b143485a4e47529199266
DESKTOP-8FE64MQ\Rini Kurriana Putri <kurriana@gmail.com>
2017-04-29 12:13:11 +0700

    tampilan login error

--- application/views/login_error_view.php
+++ application/views/login_error_view.php
@@ -2,8 +2,14 @@
 <html>
 <head>
     <title>Login Error</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="user-scalable=0, width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Belle &amp; Carrie Rehabilitation Yoga Web Template</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css')?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/mobile.css')?>">
+	<script type='text/javascript' src='<?php echo base_url('assets/js/mobile.js')?>'></script>
 </head>
 <body>
-<h2>Login Error!</h2>
+<font face="Times New Roman"><font color="#FFFFFF"><h3>Login Error! <a href ='login'>Back</a></h3></font></font>
 </body>
 </html>
