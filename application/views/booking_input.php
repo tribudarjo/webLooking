367a1579bfed8d7b545a6e66091862be1065889e
TRISATYO <tribudarjo@gmail.com>
2017-05-10 10:47:02 +0700

    commit menu di booking

--- application/views/booking_input.php
+++ application/views/booking_input.php
@@ -1,8 +1,33 @@
 <html>
 <head>
 <title>Booking | LOOKING</title>
+<meta charset="UTF-8">
+	<meta name="viewport" content="user-scalable=0, width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css')?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/menu.css')?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/mobile.css')?>">
+	<script type='text/javascript' src='<?php echo base_url('assets/js/mobile.js')?>'></script>
+		<script type='text/javascript' src='<?php echo base_url('assets/js/menu.js')?>'></script>
 </head>
 <body>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<div id="wrapper" class="active">  
+
+    <div id="sidebar-wrapper">
+        <ul id="sidebar_menu" class="sidebar-nav">
+           <li class="sidebar-brand"><a id="menu-toggle" href="#">LOOKING<i class="fa fa-home"></i></a></li>
+        </ul>
+        <ul class="sidebar-nav" id="sidebar">
+          <li><a href="#">Member<span class="fa fa-user"></span></a></li>
+          <li><a href="#">Booking<span class="fa fa-car"></span></a></li>
+          <li><a href="#">Contact US<span class="fa fa-search"></span></a></li>
+        </ul>
+      </div>
+          
+     
+      
+</div>
+<div class="container">
 <h1>Booking | LOOKING</h1>
 <form action="<?php echo base_url().'booking/tambah_aksi'; ?>" method="post">
 <table style="margin:20px auto;">
@@ -71,9 +96,10 @@
 	</td>
 </tr>
 <tr>
-<td><input type="submit" name="submit" value="Booking"></td>
+<td align="center" colspan="3"><input type="submit" name="submit" value="Booking"></td>
 </tr>
 </table>
 </form>
+</div>
 </body>
 </html>
