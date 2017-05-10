85f0aef50fc8b5f085188201aa0d6969dc06ab27
TRISATYO <tribudarjo@gmail.com>
2017-05-10 07:25:00 +0700

    edit title

--- application/views/login_error_view.php
+++ application/views/login_error_view.php
@@ -4,7 +4,6 @@
     <title>Login Error</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="user-scalable=0, width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<title>Belle &amp; Carrie Rehabilitation Yoga Web Template</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css')?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/mobile.css')?>">
 	<script type='text/javascript' src='<?php echo base_url('assets/js/mobile.js')?>'></script>
--- application/views/login_success_view.php
+++ application/views/login_success_view.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Login Error</title>
+<title>Login Success</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="user-scalable=0, width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<title>Belle &amp; Carrie Rehabilitation Yoga Web Template</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css')?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/mobile.css')?>">
 	<script type='text/javascript' src='<?php echo base_url('assets/js/mobile.js')?>'></script>
