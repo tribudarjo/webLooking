492fd0d7157a448dc88d9dd9b11015e64ccc3a86
TRISATYO <tribudarjo@gmail.com>
2017-05-23 20:59:27 +0700

    merge, sudah tak simpen backupnya

--- application/controllers/booking.php
+++ application/controllers/booking.php
@@ -1,23 +1,35 @@
 <?php
 class booking extends CI_Controller{
+	public $model = NULL;
 	function __construct(){
 		parent::__construct();
-		$this -> load -> model('Model_booking');
-		if(empty($this -> session -> userdata('id_user'))){
-			$this -> session -> set_flashdata('flash_data','Anda tidak mempunyai akses!');
-			redirect('login');
-		}
+		$this->load->model('login_model');
+		$this-> model = $this->login_model;
 	}
 	
 	public function index(){
-		$this -> load -> view('booking_input');
-		$id_mobil = $this -> load -> post('id_mobil');
-		$waktu_sewa_jam = $this -> input -> post('waktu_sewa_jam');
-		$waktu_kembali_jam = $this -> input -> post('waktu_kembali_jam');
-		$this -> Model_booking -> cek_harga($id_mobil, $waktu_sewa_jam, $waktu_kembali_jam);
+		$this -> isLogin();
 	}
 	
+	public function isLogin(){
+	if(isset($_SESSION['email']) && isset($_SESSION['password'])){
+			$this->model->email = $_SESSION['email'];
+			$this->model->password = $_SESSION['password'];
+			$cek = $this->model->login();
+			if($cek == 0){
+				$this->load->view('login_form_view');
+				unset($_SESSION['email']);
+				unset($_SESSION['password']);
+			}else{
+				
+				$this->load->view('booking_input');
+			}
+		}else{
+			$this->load->view('login_form_view');
+		}
+	}
 	function tambah_aksi(){
+		
 		$mobil = $this -> input -> post('nama_mobil');
 		$tanggal_sewa = $this -> input -> post('tgl_sewa');
 		$waktu_sewa_jam = $this -> input -> post('waktu_sewa_jam');
@@ -27,10 +39,10 @@ class booking extends CI_Controller{
 		$waktu_kembali_jam = $this -> input -> post('waktu_kembali_jam');
 		$waktu_kembali_menit = $this -> input -> post('waktu_kembali_menit');
 		$waktu_kembali = $waktu_kembali_jam.":".$waktu_kembali_menit;
-		$id_member = $this -> session -> userdata('id_member');
+		//$id_member = $_SESSION['id_member'];
 		$data1 = array(
 		'id_mobil' => $mobil,
-		'id_member' => $id_member,
+		//'id_member' => $id_member,
 		'tanggal_sewa' => $tanggal_sewa,
 		'tanggal_kembali' => $tanggal_kembali,
 		'jam_pinjam' => $waktu_sewa,
@@ -40,15 +52,10 @@ class booking extends CI_Controller{
 		$this -> load -> model('Model_booking');
 		$this -> Model_booking -> input_data($data1,'booking');	
 		redirect('booking/index');
+		
 	}
 
-	public function logout(){
-		$data = ['email', 'password'];
-		$this -> session -> unset_userdata($data);
-		 $this->session->sess_destroy();
-		
-		redirect('login');
-	}
+	
 
 }
 ?>
