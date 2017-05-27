5c69874c2289e207ff99a4042eea2ef693b2d4d3
Dewi\itikaf <dewiitikaf@gmail.com>
2017-05-28 01:38:23 +0700

    tampilannya sudah diganti

--- application/views/profil/Profil_create_view.php
+++ application/views/profil/Profil_create_view.php
@@ -7,19 +7,19 @@
 
 	<form action="create" method="POST">
 		<?php echo $model->labels['id_member'];?><br/>
-		<input type="text" name="kode" size="10" maxlength="10"/><br/><br/>
+		<input type="text" name="id_member" size="10" maxlength="10"/><br/><br/>
 		
 		<?php echo $model->labels['nama_member'];?><br/>
 		<input type="text" name="nama" size="30" maxlength="25"/><br/><br/>
 		
 		<?php echo $model->labels['email'];?><br/>
-		<textarea name="email"></textarea><br/><br/>
+		<input type="text" name="email"><br/><br/>
 		
 		<?php echo $model->labels['no_hp'];?><br/>
-		<textarea name="stok"></textarea><br/><br/>
+		<input type="text" name="no_hp"> <br/><br/>
 		
 		<?php echo $model->labels['password'];?><br/>
-		<textarea name="stok"></textarea><br/><br/>
+		<input type="password" name="password"><br/><br/>
 		
 		<input type="submit" name="btnSubmit" value="Simpan"/>
 		<input type="button" value="Batal" onclick="javascript:history.go(-1);"/>
--- application/views/profil/Profil_update_view.php
+++ application/views/profil/Profil_update_view.php
@@ -8,19 +8,19 @@
 	
 	<form action="create" method="POST">
 		<?php echo $model->labels['id_member'];?><br/>
-		<input type="text" name="id_member" size="10" maxlength="10" value="<?php echo  $model->id_member;?>"/><br/><br/>
+		<input type="text" name="id_member" size="10" maxlength="10" value="<?php echo  $model->id_member;?>"/><br/>
 		
 		<?php echo $model->labels['nama_member'];?><br/>
-		<input type="text" name="nama" size="30" maxlength="25" value="<?php echo  $model->nama_member;?>"/><br/><br/>
+		<input type="text" name="nama" size="30" maxlength="25" value="<?php echo  $model->nama_member;?>"/><br/>
 		
 		<?php echo $model->labels['email'];?><br/>
-		<textarea name="email"><?php echo $model->email;?></textarea><br/><br/>
+		<input type="text" name="email"value="<?php echo $model->email;?>"><br/>
 		
 		<?php echo $model->labels['no_hp'];?><br/>
-		<textarea name="no_hp"><?php echo $model->no_hp;?></textarea><br/><br/>
+		<input type="text" name="no_hp" value="<?php echo $model->no_hp;?>"><br/>
 		
 		<?php echo $model->labels['password'];?><br/>
-		<textarea name="password"><?php echo $model->password;?></textarea><br/><br/>
+		<input type="password" name="password" value="<?php echo $model->password;?>"><br/><br/>
 		
 		<input type="submit" name="btnSubmit" value="Simpan"/>
 		<input type="button" value="Batal" onclick="javascript:history.go(-1);"/>
