2aa888f8d407f0fb89253d8a34562e14cb5f0e57
DESKTOP-8FE64MQ\Rini Kurriana Putri <kurriana@gmail.com>
2017-05-28 12:58:32 +0700

    coba commit view register

--- application/views/v_register.php
+++ application/views/v_register.php
@@ -5,7 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
    <meta charset="UTF-8">
-   <title>  REGISTER  </title>
+   <title>  Pendaftaran Akun  </title>
  
  <meta charset="UTF-8">
 	<meta name="viewport" content="user-scalable=0, width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
