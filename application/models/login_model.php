e670c19a68d2d5d6997c4be5fd9fee943473b8df
TRISATYO <tribudarjo@gmail.com>
2017-05-28 13:24:22 +0700

    pembaruan login, ganti dari result ke num_rows

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -17,7 +17,7 @@ class login_model extends CI_model {
 	public function login(){
 		$sql = "SELECT*FROM member WHERE email='$this->email' AND password='$this->password'";
 		$query = $this->db->query($sql);
-		return $query->result();
+		return $query->num_rows();
 	}
 	
 	public function read(){
