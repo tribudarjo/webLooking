6921832bb5c684036ed7a995dcd3afba55960dba
DESKTOP-8FE64MQ\Rini Kurriana Putri <kurriana@gmail.com>
2017-05-28 13:05:53 +0700

    tampilan daftar mobil

--- application/views/v_deskripsi.php
+++ application/views/v_deskripsi.php
@@ -6,49 +6,55 @@ else {
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<head>
-<meta charset="UTF-8">
-	<meta name="viewport" content="user-scalable=0, width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<title>Daftar Mobil</title>
+	
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css')?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/styledeskripsi.css')?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/mobile.css')?>">
 	<script type='text/javascript' src='<?php echo base_url('assets/js/mobile.js')?>'></script>
 </head>
 <body>
-<div class="container">
-<h1 class="w3-animate-top">Daftar Mobil |<i class="fa fa-car" style="font-size:24px"></i></h1><br/><br/>
-<table>
+<div class="w3-container">
+<h2>Daftar Mobil</h2>
 
-<table class="table table-condensed">
+<div class="col-md-5 col-sm-4 col-xs-6"> 
+    <table width="80%" class="w3-table"> 
+	<style>
+  table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 2px solid #ddd;
+}
+</style>
+</head>
     <thead>
       <tr>
+ <th> Gambar Mobil </th>
  <th> Nama Mobil </th>
  <th> Harga </th>
  <th> Deskripsi </th>
- <th> Gambar Mobil </th>
  <th> Tahun </th>
  <th> Nama Rental </th>
 </tr>
     </thead>
 </div>
+</div>
 <?php
 $id = 101;
 foreach ($hasil as $data):
 ?>
+
 <tr>
- 
- <td> <?php echo $data->nama_mobil; ?> </td>   
+ <td>  <img src="<?php echo base_url($data->gambar_mobil) ?>"> </td>
+ <td> <?php echo $data->nama_mobil; ?> </td>
  <td> <?php echo $data->harga; ?> </td>  
  <td> <?php echo $data->deskripsi; ?> </td>
- <td>  <img src="<?php echo base_url($data->gambar_mobil) ?>"> </td>
  <td> <?php echo $data->tahun; ?> </td>
  <td> <?php echo $data->nama_rental; ?> </td>
 
@@ -59,6 +65,7 @@ $id++;
 endforeach;
 ?>
 </table>
+</div>
 <?php
 }
 ?>
