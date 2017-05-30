8b99086f49cea185118e32cf9115bbe85c356030
Rini Kurriana Putri <kurriana@gmail.com>
2017-05-30 10:46:48 +0700

    ganti lagi

--- application/views/v_register.php
+++ application/views/v_register.php
@@ -1,44 +1,31 @@
- <?php
- defined('BASEPATH') OR exit('No direct script access allowed');
- ?><!DOCTYPE html>  
- <head>
-   <meta charset="UTF-8">
-   <title>  REGISTER  </title>
- 
- <meta charset="UTF-8">
+<html>
+<head>
+<title>Login | LOOKING</title>
+	<meta charset="UTF-8">
 	<meta name="viewport" content="user-scalable=0, width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<title>Belle &amp; Carrie Rehabilitation Yoga Web Template</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css')?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/mobile.css')?>">
 	<script type='text/javascript' src='<?php echo base_url('assets/js/mobile.js')?>'></script>
- 
 
- <meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-
-		<!-- Website CSS style -->
-		<link rel="stylesheet" type="text/css" href="assets/css/main.css">
-
-		<!-- Website Font style -->
-	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
-		
-		<!-- Google Fonts -->
-		<link href='https://fonts.googleapis.com/css?family=Passion+One' rel='stylesheet' type='text/css'>
-		<link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
 </head>
- <body>
- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-<div class="main">
- 
-     
-	 <center><div class="container">
-	 <div class="middle">
-	 <div id="login">
-	             <div class="row">
 
- <fieldset class="clearfix">
- 
-     <?php echo form_open('register');?>
+<body>
+
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<div class="main">
+    
+    
+    <div class="container">
+<center>
+<div class="middle">
+      <div id="login">
+	  
+	 
+
+       
+          <fieldset class="clearfix">
+
+             <?php echo form_open('register');?>
      <p>Nama:</p>
      <p>
 	 <span class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i></span>
@@ -56,7 +43,7 @@
 	   <p>Phone:</p>
      <p>
 	 <span class="input-group-addon"><i class="fa fa-phone" aria-hidden="true"></i></span>
-     <input type="text" name="no_hp" value="<?php echo set_value('no_hp'); ?>" placeholder="Enter your Phone"/> 
+     <input type="text" name="no_hp" value="<?php echo set_value('no_hp'); ?>" placeholder="Enter your Phone"/>
      </p>
      <p> <?php echo form_error('no_hp'); ?> </p>
  
@@ -85,17 +72,36 @@
      <p>
      Kembali ke menu login, Silakan klik <?php echo anchor(site_url().'/login','di sini..'); ?>
      </p>
- <div class="clearfix"></div>
 
-      </div>
-      <div class="logo"> <img src='../assets/images/FIXX Copy.png' height='190px' width='190px'> 
-</body>
-          
+          <ul>
+  
+             <div>
+                              
+                            </div>
+
+          </fieldset>
+		  
+<div class="clearfix"></div>
+        </form>
+
+        <div class="clearfix"></div>
+   <div class="clearfix"></div>
+      <div class="clearfix"></div>
+	     <div class="clearfix"></div>
+      </div> <!-- end login -->
+	       <div class="clearfix">
+		   <br><br><br><br><br><br><br><div class="logo"> <img src='../assets/images/FIXX Copy.png' height='190px' width='200px'> </div> 
+		   </div>
+		  
+            
+          </div>
           <div class="clearfix"></div>
       </div>
       
       </div>
+	
 </center>
+
     </div>
 
 </div>
