d7f070887975ee08b4a682df84ab336e9953c488
Rini Kurriana Putri <kurriana@gmail.com>
2017-05-31 08:55:39 +0700

    tampilan syarat ketentuan done siap tempur

--- application/views/syarat_ketentuan_view.php
+++ application/views/syarat_ketentuan_view.php
@@ -1,10 +1,17 @@
 <html>
 <head>
 <title>Syarat & Ketentuan | LOOKING</title>
+
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css')?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/menu.css')?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/mobile.css')?>">
+	<script type='text/javascript' src='<?php echo base_url('assets/js/mobile.js')?>'></script>
+	<script type='text/javascript' src='<?php echo base_url('assets/js/menu.js')?>'></script>
 </head>
 
-<body>
-<h1>SYARAT & KETENTUAN</h1>
+<body><br><br><br><br>
+<center><h1>SYARAT & KETENTUAN</h1></center>
+<br>
 <table>
 <tr><td>1.</td><td>Waktu kerja 24 jam.</td></tr>
 <tr><td>2.</td><td>Bebas jarak tempuh.</td></tr>
@@ -13,4 +20,7 @@
 <tr><td>5.</td><td>Waktu peminjaman mobil terhitung mulai jam berangkat.</td></tr>
 </table>
 </body>
+</br>
+</br>
+</br></br></br>
 </html>
